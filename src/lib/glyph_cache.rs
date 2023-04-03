65ba0795dcdfa5809e49ad68ca2f93ec6eba76da
Dave O <davidobrite@gmail.com>
2023-04-02 18:33:25 -0700

    fix(cache): fix name

--- src/lib/glyph_cache.rs
+++ src/lib/glyph_cache.rs
@@ -16,7 +16,7 @@ impl GlyphCache {
 
     fn load_glyphs(self, code_points: i32) {}
 
-    fn get_glyphs(self, code_point: i32) -> i32 {
+    fn get_glyph(self, code_point: i32) -> i32 {
         1
     }
 }
